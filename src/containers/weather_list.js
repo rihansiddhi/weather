50671da73c02e01f180220a6472f6ca0f8447c67
rihan.s <rihan.s@rently.com>
2018-12-13 11:19:14 +0530

    Completed weather project

---
+++ src/containers/weather_list.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+    renderWeather(cityData){
+        if(cityData==undefined)
+        return null;
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
+                <td>
+                    <Chart data={pressure} color="green" />
+                </td>
+                <td>
+                    <Chart data={humidity} color="purple" />
+                </td>
+            </tr>
+        );
+    }
+    render() { 
+        return ( 
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.props.weather.map(this.renderWeather) }
+                </tbody>
+            </table>
+         );
+        }
+    }
+    
+    function mapStateToProps({ weather })
+    {
+        return { weather };
+    }
+    
+    export default connect(mapStateToProps)(WeatherList);
+    // if(!cityData)
+    // return window.alert('Please enter valid city name');
+    // else
+    {/* <Sparklines height={120} width={180} data={temps}>
+    <SparklinesLine color="red" />
+    // import { Sparklines, SparklinesLine } from 'react-sparklines';
+    </Sparklines> */}
